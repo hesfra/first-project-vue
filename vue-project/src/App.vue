5f902141a188c7afc5c63e185210f5f3c7484906
Hesfra <contato.chfreire@gmail.com>
2022-05-22 19:06:45 -0300

    new project, in inicial phase

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>testing</h1>
+  <primeiroComponente />
+</template>
+
+<script>
+import primeiroComponente from './components/Primeirocomponente.vue'
+export default {
+name:'App',
+components:{
+  primeiroComponente
+}
+}
+</script>
+
+<style>
+h1{
+  font-size: 3.2rem;
+}
+</style>
---
+++ vue-project/src/components/Primeirocomponente.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>Another test</h1>
+  <p>Meu nome é {{name}}</p>
+  <p>eu trabalho como: {{profission}}</p>
+</div>
+</template>
+
+<script>
+export default {
+name: 'primeiroComponente',
+data(){
+  return{
+    name: "Christian",
+    profission: "Dev Jr."
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
