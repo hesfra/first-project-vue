1da8a653a9cfe279bc159ec1a0a20c55620eba13
Hesfra <contato.chfreire@gmail.com>
2022-05-23 15:20:30 -0300

    Another test

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/first-project-vue/',
+  base: '/first-project-vue/vue-project/',
   plugins: [vue()],
   resolve: {
     alias: {
